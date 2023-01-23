ab6b370d808f21bd7122fdd113b4195072096b4d
lissalad <elissalaymon@gmail.com>
2023-01-23 11:36:47 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import data, {
+  allCategories,
+  categoriesUnique,
+  categoriesWithCounts,
+  namesAndCategories,
+} from "./data";
+import { useState } from "react";
+import Buttons from "./components/Buttons";
+import ProductList from "./components/ProductList";
 
 function App() {
+  const [category, setCategory] = useState("All");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 id="header">Online Store</h1>
+      <main>
+        <Buttons setCategory={setCategory} />
+        <ProductList category={category} />
+      </main>
+    </>
   );
 }
 
